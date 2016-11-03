cd12add350de092a52fa623cf6886af08369bef5
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2016-11-03 16:48:44 +0100

    people for dissertations fixed

--- info/dissertation.php
+++ info/dissertation.php
@@ -12,7 +12,9 @@ $content='
 <h1 align="center">Dissertations</h1>
 
 <p>The following list of dissertations was compiled from announcements of the
-German Fachgruppe in their CA Rundbrief.  </p>
+German Fachgruppe in their CA Rundbrief.  Note that for performance the people
+URIs are resolved from the file <a
+href="http://symbolicdata.org/rdf/People.rdf" >rdf/People.rdf</a>. </p>
 
 '.dissertationen().'
 </div>
