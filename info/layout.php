58a1013c3a88835ae142a39508986a2b7e818cad
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2018-08-30 18:26:33 +0200

    wiki link fixed

--- info/layout.php
+++ info/layout.php
@@ -15,7 +15,6 @@ function pageHeader()
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="description" content="SymbolicData Standalone Info Page"/>
     <meta name="author" content="SymbolicData Project"/>
-    <link rel="icon" href="img/favicon.ico"/>
 
     <title>SymbolicData Standalone Info Page</title>
     <!-- Bootstrap core CSS -->
@@ -56,16 +55,16 @@ function generalContent()
 <div class="container">
   <h1 align="center">The CASN Demonstration Site</h1>
 
-<p> Within the <a href="http://wiki.symbolicdata.org">SymbolicData Project</a>
+<p> Within the <a href="http://symbolicdata.org">SymbolicData Project</a>
 we started the <strong>CASN subproject</strong> &ndash; Computer Algebra Social
 Network &ndash; as a germ of a “Facebook like“ communication infrastructure
 about the scientific activities within the CA community using a modern RDF
 based "Web 2.0" approach.  We refer to our wiki for <a
-href="http://wiki.symbolicdata.org/CASN" >more information about CASN goal and
+href="http://symbolicdata.github.io/CASN" >more information about CASN goal and
 concepts</a>.</p> </div> 
 
 <div class="footer"> <div class="container"> <p class="text-muted">&copy; <a
-href="http://symbolicdata.org">SymbolicData Project</a> 2015-2016 </p> </div>
+href="http://symbolicdata.org">SymbolicData Project</a> 2015-2018 </p> </div>
 </div>
 ';
 }
